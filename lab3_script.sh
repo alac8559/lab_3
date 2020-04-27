c80041d5855f23aa26ccee05e10a86adca85a4a4
alac8559 <alac8559@colorado.edu>
2020-04-27 02:12:21 -0600

    Updated Lab

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Authors : Alexis Acevedo
-# Date: 2/7/2020
+# Date: 4/27/2020
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
@@ -9,4 +9,8 @@ echo "Please input a file name"
 read filename
 echo "Please input a RegEx"
 read reg
-egrep "$reg" $filename >> email_results.txt
+grep $reg $filename
+egrep -c "[0-9]{3}-[0-9]{3}-[0-9]{4}" regex_practice.txt #count how many numbers there are in regex_practice.txt
+egrep -c ".+@.+" regex_practice.txt #count how many emails there are in regex_practice.txt
+egrep -o "303-[0-9]{3}-[0-9]{4}" regex_practice.txt #list of all phone numbers starting with 303
+egrep -o ".+@geocities.com" regex_practice.txt >> email_results.txt #will store all emails ending in geocities to email_results
